21fa1e8858a40b68a1fc27be096a73f69c02b22d
Maryan62290 <78801729+Maryan62290@users.noreply.github.com>
2021-04-12 16:14:45 +0200

    Add files via upload

---
+++ portfolio/classes/includes/footer.php
@@ -0,0 +1,13 @@
+<script src="../../js/jquery-3.5.1.min.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/all.min.js"></script>
+</body>
+<script>
+	$("nav").show();
+	$(".show").click(function(){
+			$( "nav" ).slideToggle(500);
+	
+		});
+			
+</script>
+</html>
---
+++ portfolio/classes/includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1  shrink-to-fit=no" />
+    <link rel="stylesheet" href="../../css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../../css/all.min.css" />
+    <link rel="stylesheet" href="../../css/index.css" />
+    <title></title>
+</head>
+<body>
+    <nav class="text-center ">
+        <a class="scroll-link" href="">Top</a>
+        <a class="scroll-link" href="articles/">Articles</a>
+        <a class="scroll-link" href="boutique/">Boutique</a>
+        <a class="scroll-link" href="portfolio/">Portfolio</a>
+        <a class="scroll-link" href="">About</a>
+        <a class="scroll-link" href="contact">Contact</a>
+                    <!--<div class="hide my-auto">
+                        <i class="fa fa-bars "></i>
+                    </div><!-- hide -->
+    </nav>
+	<div class="show my-auto">
+		<i class="fa fa-bars "></i>
+	</div><!-- show -->
