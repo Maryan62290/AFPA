d7621253d4ab51c145b422cafce3f4c57c60afa9
Maryan62290 <78801729+Maryan62290@users.noreply.github.com>
2021-04-20 18:34:57 +0200

    Update CreationUser.php

--- portfolio/classes/controller/CreationUser.php
+++ portfolio/classes/controller/CreationUser.php
@@ -1,23 +1,26 @@
 <?php
    
-    require "../includes/header.php";
+    require (__DIR__ . "/../includes/header.php");
     require_once  "../view/ViewUser.php" ;
     require_once  "../model/ModelUser.php";
+    require_once  "../controller/UploadPhoto.php";
+
     
-
-    if (isset($_POST['ajout'])) {
-
-        ModelUser::ajoutUser($_POST['nom'], $_POST['prenom'],$_POST['mail'], $_POST['tel'], $_POST['adresse'], $_POST['photo'], $_POST['description']);
-
-    ?>
+    if (isset($_POST['ajout']) && UploadPhoto::AjoutImage() == True) {
+        
+        ModelUser::ajoutUser($_POST['nom'], $_POST['prenom'],$_POST['mail'], $_POST['tel'], $_POST['adresse'], $_FILES['photo'], $_POST['description']);
+            
+?>
         <div class="alert alert-success" role="alert">
-            simple Alert réussie !!
+            Utilisateur Ajouté et Upload de l'image terminé !! 
         </div>
-    <?php
+<?php
     } else {
+
         ViewUser::ajoutUser();
     }
 
-    require "../includes/footer.php";
+    require (__DIR__ . "/../includes/footer.php");
 
 ?>
+
